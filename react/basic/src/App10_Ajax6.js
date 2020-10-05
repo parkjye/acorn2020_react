bd3b9f85cf31df74aa82fe531f5e325984574ed3
parkjye <parkjye44@gmail.com>
2020-10-05 12:51:39 +0900

    <Feat> basic :: query-string 활용한 form data 처리

---
+++ react/basic/src/App10_Ajax6.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+// >yarn add query-string 입력(설치) 후에 사용할 수 있다.
+import qs from 'query-string';
+
+const App10_Ajax6=()=>{
+    //상태값
+    const [formData, setFormData]=useState({id:'', pw:''});
+
+    const changed=(e)=>{
+        //이벤트가 일어난 input 요소의 name 속성의 값(id or pwd) 읽어오기
+        let name=e.target.name;
+        
+        //이벤트가 일어난 input 요소에 입력한 value
+        let value=e.target.value;
+
+        setFormData({
+            ...formData, //formData 내용을 Object에 값을 나열 후 
+            [name]:value //[name]이라는 방 이름으로 값을 오버라이드
+            //[e.target.name]:e.target.value 와 같은 형식으로 작성한다.
+        });
+    };
+
+    //form의 submit버튼을 클릭하면
+    //(form에 submit이벤트 발생->) const submit로 실행순서가 들어온다.
+    const submit=(e)=>{
+
+        //폼 전송 막기
+        e.preventDfault();
+
+        fetch("http://localhost:8888/spring05/users/ajax_login.do", {
+            method:"POST",
+            headers:{"Content-Type":"application/x-www-form-urlencoded"},
+            body:qs.stringify(formData) //form에 입력한 (Object)data를 쿼리 문자열로 변경해서 보낸다.
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+        })
+        .catch(err=>{
+            console.log(err);
+        });
+    };
+
+    return (
+        <div>
+            <h1>로그인 폼 입니다.</h1>
+            <form onSubmit={submit}>
+                <input onChange={changed} name="id" type="text" placeholder="아이디" />
+                <input onChange={changed} name="pwd" type="password" placeholder="비밀번호" />
+                <button type="submit">로그인</button>
+            </form>
+            <p>{JSON.stringify(formData)}</p>
+            <p>{qs.stringify(formData)}</p>
+        </div>
+    );
+};
+
+export default App10_Ajax6;
