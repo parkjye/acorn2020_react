7d7992b080b17d72c7180fafb419440ae739b55c
parkjye <parkjye44@gmail.com>
2020-10-05 11:00:12 +0900

    <Style> basic :: useEffect()에 대한 설명 추가

--- react/basic/src/App10_Ajax4.js
+++ react/basic/src/App10_Ajax4.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/bootstrap.css';
 
 const App10_Ajax4=()=>{
     
     //초기값 전달 [초기값, 초기값을 바꿀 함수]
-    const [cafeList, setList] = React.useState([]);
+    //React.useState([]); 보다 useState를 import해서 사용하는 것이 좋다.
+    const [cafeList, setList] = useState([]);
 
     //componentDidMount() 대신 사용하는 useEffect() 함수
-    //아래에 전달한 화살표 함수는 컴포넌트가 마운트 직후에 호출된다.
+    //아래에 전달한 화살표 함수는 컴포넌트가 마운트 된 직후에 호출된다.
     useEffect(()=>{
         fetch("http://localhost:8888/spring05/cafe/ajax_list.do")
         .then(res=>res.json())
